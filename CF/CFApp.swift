879cb0977e8c4c81521abb04e2fa99fd1a3d2a1e
Pistoy <emrecekic73@gmail.com>
2024-07-26 10:45:21 +0300

    Initial Commit

---
+++ CF/CFApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFApp.swift
+//  CF
+//
+//  Created by Emre Cekic on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CF/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CF
+//
+//  Created by Emre Cekic on 26.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
